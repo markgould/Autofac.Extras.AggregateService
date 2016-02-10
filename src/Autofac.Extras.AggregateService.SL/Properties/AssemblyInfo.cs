f87e27752326cdf0fc00e80090967e0f477a7522
Mark Gould <markg@spindlemedia.com>
2016-02-10 11:11:08 -0600

    Add SL project

---
+++ src/Autofac.Extras.AggregateService.SL/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Autofac.Extras.AggregateService.SL")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
+[assembly: AssemblyDescription("Dynamic aggregate service implementation generation for Autofac.")]
+
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyInformationalVersion("4.0.0.0")]
